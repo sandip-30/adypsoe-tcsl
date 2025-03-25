1102aa47b17b5907a462158f9aa92d6f9244f960
sandip-30 <mesandipkamble@gmail.com>
2025-03-25 16:28:49 +0530

    second commit 25042025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TopHeaderComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
