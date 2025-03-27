51c252428f3c127f1176f73db3be99fa144f2b21
sandip-30 <mesandipkamble@gmail.com>
2025-03-27 16:56:00 +0530

    carousel services created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { WhyUsComponent } from './why-us/why-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TopHeaderComponent,HeaderComponent],
+  imports: [TopHeaderComponent,HeaderComponent,CarouselComponent,OurServicesComponent,WhyUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
